2e0b11004152f394909ca1254fff14f24e80d90b
OlgaShreyn <57268388+OlgaShreyn@users.noreply.github.com>
2023-04-13 21:53:21 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+import scipy.stats as st
 
 
 chat_id = 368780632 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    p = ttest_ind(x,y).pvalue
-    if(p<0.09):
-      return True
-    else:
-      return False
+    t_statistic, p_value = st.ttest_1samp(x, 500, alternative="two-sided")
+    return p_value < 2*0.09 and x.mean() > 500
