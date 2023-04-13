6ba5118e988333df585554fbfa024beddf9312b6
OlgaShreyn <57268388+OlgaShreyn@users.noreply.github.com>
2023-04-13 11:12:02 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ from scipy.stats import ttest_ind
 
 chat_id = 368780632 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     p = ttest_ind(x,y).pvalue
     if(p<0.09):
       return True
